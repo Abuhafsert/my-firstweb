89f9bd5f431e01faf303ce15cf643c6942573ea1
Abuhafsert <153815643+Abuhafsert@users.noreply.github.com>
2024-01-01 06:19:57 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,155 @@
+
+
+
+
+
+// const teachs = new Fall();
+// teachs.teach();
+
+// reach.rich()
+
+// show(2);
+// showHideUser(2);
+// change();
+// sum();
+
+
+
+
+
+
+let header = document.querySelector(".list")
+
+function showHideUser() {
+    header.classList.toggle("hide")
+}
+let balance = 5000
+
+let amountPerThousand = 1100
+let total = amountPerThousand / 1000
+
+let orderForm = document.querySelector(".orderform")
+let userSubmit = document.querySelector(".usersubmit")
+document.getElementById("listnair").innerHTML = balance;
+
+let amountPer = document.querySelector(".wayene");
+let placeHOld = document.querySelector(".totalAmoun")
+
+// orderForm.addEventListener("click", function(e) {
+//     if(userSubmit) {
+//         e.preventDefault()
+//     } 
+// }) 
+
+
+let guarant = document.querySelector(".serving")
+
+let guaranty = document.getElementById("services")
+let waranty = document.getElementById("servic")
+let ranty = document.getElementById("firstService")
+let aranty = document.getElementById("secondService")
+let wanty = document.getElementById("thirdService")
+let arnty = document.getElementById("fourtService")
+
+
+
+guarant.addEventListener('change', function(event) {
+
+    if(event.target.value !== '1') {
+        guaranty.style.display = 'none'
+    } else {
+        guaranty.style.display = 'block'
+    }
+    if(event.target.value == 2) {
+        waranty.style.display = 'block'
+    } else {
+        waranty.style.display = 'none'
+    }
+    if(event.target.value == 3) {
+        ranty.style.display = 'block'
+    } else {
+        ranty.style.display = 'none'
+    }
+    if(event.target.value == 4) {
+        aranty.style.display = 'block'
+    } else {
+        aranty.style.display = 'none'
+    }
+    if(event.target.value == 5) {
+        wanty.style.display = 'block'
+    } else {
+        wanty.style.display = 'none'
+    }
+    if(event.target.value == 6) {
+        arnty.style.display = 'block'
+    } else {
+        arnty.style.display = 'none'
+    }
+}); 
+
+
+
+guarant.addEventListener('inner', function(e) {
+    if(e.target.value == 1) {
+        console.log(guarant.innerHTML)
+        
+    }
+})
+
+
+
+
+let userr = document.querySelector(".user")
+let useramount = document.querySelector(".useramount")
+
+let containt = document.querySelector(".containt")
+
+let dat = new Date();
+
+function totals() {
+
+}
+
+
+function valueUser() {
+
+    if(!userr.value || !useramount.value) {
+        containt.firstElementChild.innerHTML = "incorrect link"
+        containt.style.display = "flex" 
+    } else if(useramount.value <= 20){
+        containt.innerHTML = "maximum order is 20"
+        containt.style.display = 'flex'
+    } else if (useramount.value >= balance){
+        containt.innerHTML = 'not enough balance'
+        containt.style.display = 'flex'
+    } else if(useramount.value >= 2500) {
+        containt.innerHTML = 'maximum order is 2500'
+        containt.style.display = 'flex'
+    } else {
+        balance -= total * useramount.value
+        containt.innerHTML = `
+        ${dat} <br> link: ${userr.value} <br> amount: ${total * useramount.value} <br> balance: ${balance}`
+        document.getElementById("listnair").innerHTML = balance;
+        containt.style.display = "flex"
+        userr.value = ""
+        useramount.value = ""
+        placeHOld.value = ""
+    }
+
+    
+}
+
+useramount.addEventListener('input', () => {
+    placeHOld.value = `NGN  ${total * useramount.value}`;
+
+})
+
+
+
+
+
+
+
+
+
+// purchase();
